71e7f441cebb2f8ec9bfd3aa15f3798548255234
Sean Kell <sean.michael.kell@gmail.com>
2017-08-08 08:44:42 -0400

    chg: Add components for current inventory and vendor pricing tables. !refactor

---
+++ src/app/vendor-pricing/vendor-pricing.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'inv-vendor-pricing',
+  templateUrl: './vendor-pricing.component.html',
+  styleUrls: ['./vendor-pricing.component.css']
+})
+export class VendorPricingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
