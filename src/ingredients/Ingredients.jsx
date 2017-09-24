fb89e7db408b48f8f217d3f7e9f17b4707f51e12
Sean Kell <sean.michael.kell@gmail.com>
2017-09-24 11:05:07 -0400

    new: Add modal form for adding new ingredient.

--- src/ingredients/Ingredients.jsx
+++ src/ingredients/Ingredients.jsx
@@ -1,28 +1,53 @@
 import React from 'react';
+import {Button, Table} from 'react-bootstrap';
+import NewIngredient from './NewIngredient';
 import './Ingredients.css'
 
-function Ingredients() {
-    return (
-        <table>
-            <thead>
-                <tr>
-                    <th>Ingredient</th>
-                    <th>Type</th>
-                    <th></th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td>Crisp Malt Wheat</td>
-                    <td>Malt</td>
-                    <td>
-                        <button>Edit</button>
-                        <button>Delete</button>
-                    </td>
-                </tr>
-            </tbody>
-        </table>
-    );
+class Ingredients extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {showModal: false};
+        this.close = this.close.bind(this);
+        this.open = this.open.bind(this);
+    }
+
+    close() {
+        this.setState({ showModal: false });
+    }
+
+    open() {
+        this.setState({ showModal: true });
+    }
+    
+    render() {
+        return (
+            <div>
+                <Table striped bordered condensed hover>
+                    <thead>
+                        <tr>
+                            <th className="center">Ingredient</th>
+                            <th className="center">Type</th>
+                            <th className="center"></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>Crisp Malt Wheat</td>
+                            <td>Malt</td>
+                            <td>
+                                <Button bsStyle="primary">Edit</Button>
+                                <Button bsStyle="danger">Delete</Button>
+                            </td>
+                        </tr>
+                    </tbody>
+                </Table>
+                <br />
+                <Button bsStyle="success" onClick={this.open}>Create</Button>
+
+                <NewIngredient show={this.state.showModal} close={() => this.close()} />
+            </div>
+        );
+    }
 }
 
 export default Ingredients;
---
+++ src/ingredients/NewIngredient.jsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import {Button, ControlLabel, FormControl, FormGroup, HelpBlock, Modal} from 'react-bootstrap';
+
+class NewIngredient extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {value: ''};
+        this.getValidationState = this.getValidationState.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    getValidationState() {
+        return 'success';
+    }
+
+    handleChange(e) {
+        this.setState({ value: e.target.value });
+    }
+    
+    render() {
+        return (
+            <Modal show={this.props.show} onHide={this.props.close}>
+                <Modal.Header closeButton>
+                    <Modal.Title>New Ingredient</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <form>
+                        <FormGroup
+                            controlId="formBasicText"
+                            validationState={this.getValidationState()}
+                        >
+                            <ControlLabel>Ingredient Name</ControlLabel>
+                            <FormControl
+                                type="text"
+                                value={this.state.value}
+                                placeholder="Enter text"
+                                onChange={this.handleChange}
+                            />
+                            <FormControl.Feedback />
+                            <HelpBlock>The name of the ingredient.</HelpBlock>
+                        </FormGroup>
+                        <Button type="submit">
+                            Submit
+                        </Button>
+                    </form>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button onClick={this.props.close}>Close</Button>
+                </Modal.Footer>
+            </Modal>
+        );
+    }
+}
+
+export default NewIngredient;
