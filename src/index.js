ddd20e568f67fdf07a2b2266aff97b0cb69d4c1e
Sean Kell <sean.michael.kell@gmail.com>
2017-09-24 17:44:55 -0400

    new: Implement interacting with an API for ingredients component.

--- src/index.js
+++ src/index.js
@@ -4,9 +4,13 @@ import { Provider } from 'react-redux';
 import { ConnectedRouter } from 'react-router-redux';
 import store, { history } from './store';
 import App from './containers/app';
+import {fetchIngredients} from './modules/ingredients';
 import registerServiceWorker from './registerServiceWorker';
 
 import './index.css';
+
+// This doesn't seem like the right place...
+store.dispatch(fetchIngredients());
 
 const target = document.getElementById('root');
 
--- src/modules/ingredients.js
+++ src/modules/ingredients.js
@@ -1,7 +1,23 @@
+import Axios from 'axios';
+
 // Action keys
 export const CREATE_INGREDIENT = 'ingredients/CREATE_INGREDIENT';
+export const CREATE_INGREDIENT_SUCCESS = 'ingredients/CREATE_INGREDIENTS_SUCCESS';
+export const FETCH_INGREDIENTS = 'ingredients/FETCH_INGREDIENTS';
+export const FETCH_INGREDIENTS_SUCCESS = 'ingredients/FETCH_INGREDIENTS_SUCCESS';
 
-// Initial state.
+// Configuration. We should find a better way to do this than hard coding paths.
+const devApi = 'http://59c8099261bea80011d40d97.mockapi.io/api/ingredient';
+const prodApi = '/api/ingredient';
+
+let api;
+if (process.env.NODE_ENV === 'development') {
+  api = devApi;
+} else if (process.env.NODE_ENV === 'production') {
+  api = prodApi;
+}
+
+// Initial state. 
 const initialState = {
   ingredients: [
     {
@@ -15,13 +31,18 @@ const initialState = {
 // Reducer function
 export default (state = initialState, action) => {
   switch(action.type) {
-    case CREATE_INGREDIENT:
+    case CREATE_INGREDIENT_SUCCESS:
       return {
         ...state,
         ingredients: [
           ...state.ingredients,
           Object.assign({}, action.ingredient)
         ]
+      };
+    case FETCH_INGREDIENTS_SUCCESS:
+      return {
+        ...state,
+        ingredients: action.ingredients
       };
     default:
       return state;
@@ -31,9 +52,42 @@ export default (state = initialState, action) => {
 // Action function
 export const createIngredient = (ingredient) => {
   return dispatch => {
+    return Axios.post(api, ingredient)
+      .then(response => {
+        // Dispatch success action
+        dispatch(createIngredientSuccess(response.data));
+      })
+      .catch(error => {
+        throw(error);
+      });
+  };
+}
+
+export const createIngredientSuccess = (ingredient) => {
+  return {
+    type: CREATE_INGREDIENT_SUCCESS,
+    ingredient
+  }
+}
+
+export const fetchIngredientsSuccess = (ingredients) => {
+  return dispatch => {
     dispatch({
-      type: CREATE_INGREDIENT,
-      ingredient: ingredient,
-    })
+      type: FETCH_INGREDIENTS_SUCCESS,
+      ingredients: ingredients,
+    });
+  };
+}
+
+export const fetchIngredients = () => {
+  return (dispatch) => {
+    return Axios.get(api)
+      .then(response => {
+        // Dispatch the success action
+        dispatch(fetchIngredientsSuccess(response.data));
+      })
+      .catch(error => {
+        throw(error);
+      });
   };
 }
